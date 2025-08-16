bc018499316f07e308489e98f113c9ddff7fd9f4
Daniela Pozo <159065844+DaniellaPR@users.noreply.github.com>
2025-08-15 20:32:42 -0500

    Add files via upload

---
+++ Ejercicios/Semana11/SisExpo/SistemaParaUnaExportadora.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sistemaparaunaexportadora;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Michael
+ */
+public class SistemaParaUnaExportadora {
+    public static void main(String[] args) {
+        NewJFrame a = new NewJFrame();
+        a.setVisible(true);
+    }
+}
